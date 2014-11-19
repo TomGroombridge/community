36d89871996f25d6f58e3a6a343038d587362788
TomGroombridge <groombridgetom@gmail.com>
2014-11-19 16:40:35 +0000

    added show action and link button

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -13,4 +13,8 @@ def index
 	@courses = Course.all
 end
 
+def show
+  @course = Course.find(params[:id])
+end
+
 end
---
+++ spec/features/show_course_feature_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+	describe 'show course' do 
+		context 'given a course' do 
+			it "should show the course in its own personal page" do 
+				visit "/courses/new"
+				fill_in 'Name', with: 'Sewing'
+				fill_in 'Description', with: "come and try sewing"
+				click_button 'Create Course'
+				
+			end
+		end
+	end
