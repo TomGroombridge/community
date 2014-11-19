267dd8897647df2b3a9ddcf8b3a5c633d0a90e9b
TomGroombridge <groombridgetom@gmail.com>
2014-11-19 19:27:13 +0000

    added user controller so a user can have a profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root "courses#index"
   
   resources :courses
+
+  match 'users/:id' => 'users#show', via: :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
