4415a7df3db2ad205787ef39b13f27168a3349cf
TomGroombridge <groombridgetom@gmail.com>
2014-11-19 14:43:41 +0000

    added correct code in controller and view to create a new course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,15 +1,16 @@
 class CoursesController < ApplicationController
 
 def new
-
+	@course = Course.new
 end
 
 def create 
+	Course.create params[:course].permit(:name, :description)
 	redirect_to '/courses'
 end
 
 def index
-	
+	@courses = Course.all
 end
 
 end
