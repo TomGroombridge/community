9be4ca66cb5bec1aa95efc72c105484c72bd1e1c
TomGroombridge <groombridgetom@gmail.com>
2014-11-26 12:58:50 +0000

    aded correct index action for finding courses belong to the dashboard owner

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,19 @@
 class DashboardController < ApplicationController
 
-def index
-	 @user = User.find params[:user_id]
-end
+	def index
+		@user = User.find params[:user_id]
+		@courses = []
+			Course.all.each do |course| 
+				if course.user_id == current_user.id
+					@courses << course  
+				end 
+			end		 
+		@courses
+	end
+
+	def course_details
+		@course = Course.find(params[:course_id])
+	end
 
 
 end
