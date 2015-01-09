7371a6843e9f051905cc8bdbbb44724526b18ec7
TomGroombridge <groombridgetom@gmail.com>
2015-01-09 11:53:29 +0000

    fixing issues with viewing course in the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,16 +3,16 @@ class DashboardController < ApplicationController
 	def index
 		@user = User.find params[:user_id]
 		@courses = []
-			Course.all.each do |course| 
-				if course.user_id == @user.id
-					@courses << course  
+			CourseDate.all.each do |course| 				
+				if course.course.user_id == @user.id
+					@courses << course 
 				end 
 			end		 
 		@courses				
 	end
 
 	def course_details
-		@course = Course.find(params[:course_id])
+		@course = CourseDate.find(params[:course_id])
 	end
 
 
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,17 +1,17 @@
 class PaymentsController < ApplicationController
 	
-	def new    				
+	def new  
     @course = CourseDate.find(params[:id])    
-    @payment = @course.payments.build    
+    @payment = @course.payments.build        
 		@payment.user = current_user
 	  @user = @payment.user	    
   end
 
   def create         
-    @payment = Payment.new(params[:payment].permit(:user_id, :course_date_id, :email))  	        
-    raise @payment.inspect
+    @payment = Payment.new(params[:payment].permit(:user_id, :course_date_id, :email, :course_id))  	            
     if @payment.save_with_payment 
-    	@payment.course.update_attribute(:quantity, @payment.course.quantity - 1)		      
+      raise @payment.inspect
+    	# @payment.course.update_attribute(:quantity, @payment.course.quantity - 1)		      
       redirect_to @payment, :notice => "Thank you for paying!"
     else
       render :new
