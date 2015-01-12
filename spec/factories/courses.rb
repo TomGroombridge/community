7faa9de430a52b7374938402903e25629d57cfb0
TomGroombridge <groombridgetom@gmail.com>
2015-01-12 15:25:29 +0000

    added course_dates to course and checked that it creates it correctly in the show.html.erb

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -2,6 +2,13 @@ FactoryGirl.define do
   factory :course do
   	name 'Learn to Bake Bread'
   	description 'This is a course where you will learn the basics of making bread'  	
-  	price 50  	  	
+  	price 50 
+  	id 1 	  	
+ 	end
+
+ 	factory :course_date do
+  	dates { rand(1..100).days.from_now }
+  	id 1 	  	
+  	course_id 1
  	end
 end
--- spec/features/create_course_feature_spec.rb
+++ spec/features/create_course_feature_spec.rb
@@ -2,16 +2,25 @@ require 'spec_helper'
 
 	describe 'create courses' do 
 
+		let(:james) { create(:course) }
+
 		before do 
 			create(:admin)
-			create(:course)			
-		end
-		
-	
+			create(:course)		
+			create(:course_date)	
+		end			
 
 		it 'should display the new course in the list of courses' do
 			visit "/"			
-			expect(page).to have_content 'Learn to Bake Bread'
+			expect(page).to have_content 'Learn to Bake Bread'			
+		end
+
+		context 'given a course' do 			
+
+			it "should be able to show a course date" do 
+				visit "/courses/1"
+				expect(page).to have_content "Book Course"
+			end
 		end
 
 end
