151e38f4b3e39bea353a4db55af3917d58971e63
TomGroombridge <groombridgetom@gmail.com>
2015-01-12 12:55:26 +0000

    added a test for a user signing up

--- spec/features/create_user_feature_spec.rb
+++ spec/features/create_user_feature_spec.rb
@@ -3,12 +3,17 @@ require 'spec_helper'
 	describe 'create a user' do 
 		context 'given a sign up page' do 
 			it 'should redirect a user to the home page for courses' do 
-				visit "/users/sign_up"
-				fill_in  'First Name', with: 'Tom'
-				fill_in  'Last Name', with: 'Groombridge'
-				fill_in 'Email', with: 'tom@example.com'
-				fill_in 'Password', with: '123456789'
-				fill_in 'Confirm Password', with: '123456789'
+				visit "/users/sign_up"				
+				save_and_open_page
+				within("#new_user") do
+					fill_in 'first_name', with: 'Tom'
+					fill_in 'last_name', with: 'Groombridge'
+					fill_in 'email', with: 'tom@example.com'
+					fill_in 'password', with: '123456789'
+					fill_in 'confirm_password', with: '123456789'
+					click_button 'SUBMIT'
+					expect(current_path).to eq '/'
+				end				
 			end			
 		end
 	end
--- spec/features/show_course_feature_spec.rb
+++ spec/features/show_course_feature_spec.rb
@@ -1,15 +1,15 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-	# describe 'show course' do 
-	# 	context 'given a course' do 
-	# 		it "should show the course in its own personal page" do 
-	# 			visit "/courses/new"
-	# 			fill_in 'Name', with: 'Sewing'
-	# 			fill_in 'Description', with: "come and try sewing"
-	# 			click_button 'Create Course'
-	# 			click_link('view')
-	# 			# expect(current_path).to eq '/courses/2'
+# 	describe 'show course' do 
+# 		context 'given a course' do 
+# 			it "should show the course in its own personal page" do 
+# 				visit "/courses/new"
+# 				fill_in 'Name', with: 'Sewing'
+# 				fill_in 'Description', with: "come and try sewing"
+# 				click_button 'Create Course'
+# 				click_link('view')
+# 				# expect(current_path).to eq '/courses/2'
 
-	# 		end
-	# 	end
-	# end
+# 			end
+# 		end
+# 	end
