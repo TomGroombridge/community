5e1e04d522d96222eb3bce38f1027d376b8d7b75
TomGroombridge <groombridgetom@gmail.com>
2015-01-13 11:25:28 +0000

    added 3 times build of need to bring so there are at least 3 text fields available to fill
    not yet working as i would like as i want to be able to send a ajax
    request to add another build if a user wants to add more text fields

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,12 +4,11 @@ def new
 	@course = Course.new	
 	@course.user = current_user
 	@course.course_addresses.build
-	@course.need_on_courses.build
+	3.times {@course.need_on_courses.build}
 end
 
 def create 		
 	@course = Course.create(params[:course].permit(:name, :description, :price, :user_id, :image, :blurb, :address, :need_to_bring, :benefits, :need_to_wear, course_addresses_attributes:[:id, :postcode], need_on_courses_attributes: [:id, :need_to_bring]))
-
 		if @course.save
 			@user =  @course.user
 			UserMailer.welcome_email(@user).deliver		
@@ -17,7 +16,6 @@ def create
 	  else
 		  format.html { render action: 'new' }	  
 		end	
-
 end
 
 def index
