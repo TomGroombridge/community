a615db67af51cc8fbc3b7e7fafed211edc350023
TomGroombridge <groombridgetom@gmail.com>
2015-02-04 17:33:49 +0000

    fixed date to start_date in tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,6 @@
 class TicketsController < ApplicationController
 	def create
 		# logic: e.g. check ticket avaailability
-		redirect_to new_payment_path(id: params[:dates], quantity: params[:quantity])
+		redirect_to new_payment_path(id: params[:start_date], quantity: params[:quantity])
 	end
 end
