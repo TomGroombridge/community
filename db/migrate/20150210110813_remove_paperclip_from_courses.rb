dfb87464eab13b11512b527acbcb9e623ac79682
TomGroombridge <groombridgetom@gmail.com>
2015-02-10 17:26:19 +0000

    remove image string

--- db/migrate/20150210110813_remove_paperclip_from_courses.rb
+++ db/migrate/20150210110813_remove_paperclip_from_courses.rb
@@ -3,7 +3,6 @@ class RemovePaperclipFromCourses < ActiveRecord::Migration
   	remove_column :courses, :image_file_name, :string
   	remove_column :courses, :image_content_type, :string
   	remove_column :courses, :image_file_size, :integer
-  	remove_column :courses, :image_updated_at, :datetime
-  	remove_column :courses, :image, :string 	  	  	
+  	remove_column :courses, :image_updated_at, :datetime  	
   end
 end
