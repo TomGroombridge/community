b937a0954acd8fd62c390f5fee90adfb999e91bf
TomGroombridge <groombridgetom@gmail.com>
2015-02-10 17:53:53 +0000

    attempt at making ckeditor working in production

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module CommunityMarketplace
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
