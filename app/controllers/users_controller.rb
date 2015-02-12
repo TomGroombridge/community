11fc4083237382093891853d61a3f679707244e2
TomGroombridge <groombridgetom@gmail.com>
2015-02-12 13:03:09 +0000

    added new avatar to user including the cropping and preview areas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,12 @@ class UsersController < ApplicationController
 	def update	
 		@user = User.find(params[:id])	
 		@user.update_attributes(params[:user].permit(:first_name, :last_name, :contact_number, :company_name, :description, :facebook, :twitter, :instagram, :google, :pinterest, :admin, :email, :avatar))
-		redirect_to current_user	
+		if params[:user][:avatar].present?
+			render :crop
+		else
+			# redirect_to course_path(@course)
+			render :preview
+		end
 	end
 
 	def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,18 @@ class User < ActiveRecord::Base
  	has_many :courses
  	has_one :subscription
  	has_one :dashboard 	
- 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
- 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
+ 	mount_uploader :avatar, AvatarUploader
+  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h 
+  after_update :crop_avatar
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def crop_avatar
+    self.avatar.recreate_versions! if crop_x.present? 
+  end
 
   def full_name
     self.first_name + " " + self.last_name
