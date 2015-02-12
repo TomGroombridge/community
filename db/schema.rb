6281c2bc0b1d085ed01e96c3fcb3d97fbe06dc15
TomGroombridge <groombridgetom@gmail.com>
2015-02-12 12:56:05 +0000

    removed and added avatar columns for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211172639) do
+ActiveRecord::Schema.define(version: 20150212125521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,10 +116,6 @@ ActiveRecord::Schema.define(version: 20150211172639) do
     t.text     "description"
     t.string   "company_name"
     t.boolean  "subscription_added",     default: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string   "company_website"
     t.string   "facebook"
     t.string   "twitter"
@@ -127,6 +123,7 @@ ActiveRecord::Schema.define(version: 20150211172639) do
     t.string   "google"
     t.string   "pinterest"
     t.boolean  "admin",                  default: false
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
