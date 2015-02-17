e497c11c3f97554f99c48302e746d7b54ad1db64
TomGroombridge <groombridgetom@gmail.com>
2015-02-17 10:26:16 +0000

    removed idle code from payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,15 +3,13 @@ class PaymentsController < ApplicationController
 	def new  
     @course_date = CourseDate.find(params[:id])    
     @payment = @course_date.payments.build        
-		@payment.user = current_user
-	  @user = @payment.user	       
+		@payment.user = current_user    
   end
 
   def create          
     @payment = Payment.new(params[:payment].permit(:course_date_id, :email, :course_id, :stripe_card_token, :full_name, :mobile_number, :special_request, :quantity))
     @course = @payment.course_date.course    
     @payment.user = current_user
-
     if @payment.save_with_payment           
     	@payment.course_date.update_attribute(:quantity, @payment.course_date.quantity - 1)		      
       redirect_to @payment, :notice => "Thank you for paying!"
