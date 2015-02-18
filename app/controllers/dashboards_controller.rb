d08722ec2e85d60416dfe0a6bc76368c5e9e7e3a
TomGroombridge <groombridgetom@gmail.com>
2015-02-18 22:38:44 +0000

    added if else statement to check payments in monthly summary

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,7 +5,7 @@ class DashboardsController < ApplicationController
 		@active_courses = @courses.all.includes(:course_dates).select do |course|
 			course.course_dates.any?(&:active?)
 		end		
-		@payments = Payment.all.select{|cd| cd.course_date.course.user_id = @user.id}		
+		@payments = Payment.all.select{|cd| cd.course_date.course.user_id == @user.id}		
 		@monthly_payments = @payments.select {|p| p.created_at >= Date.today.beginning_of_month}
 		@price = @monthly_payments.map {|p| p.price}
 		@revenue = @price.inject {|sum, n| sum += n } 		
