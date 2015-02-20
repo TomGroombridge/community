0acaefddafaf470e8d2877c09be7b55b58201491
TomGroombridge <groombridgetom@gmail.com>
2015-02-20 17:29:18 +0000

    chnaged username

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",  
     :port                 => 587,  
     :domain               => "gmail.com",  
-    :user_name            => "groombridgetom@gmail.com",  
+    :user_name            => "hello@courseiam.com",  
     :password             => Rails.application.secrets.email_password,  
     :authentication       => "plain",
     :enable_starttls_auto => true
