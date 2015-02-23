7c3a69913f552ef09a6e8b053ab6a113ce218353
TomGroombridge <groombridgetom@gmail.com>
2015-02-23 14:59:05 +0000

    reverted back

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,8 +2,8 @@ if Rails.env.development?
 	Stripe.api_key = Rails.application.secrets.secret_key
 else
 	Rails.configuration.stripe = {
-	  :publishable_key => 'pk_test_IVRbMpgxt78N8kheMNLN55Og',
-	  :secret_key      => 'sk_test_N7UZshZYAONoRj5eyV0WtH8f'
+	  :publishable_key => ENV['PUBLISHABLE_KEY'],
+	  :secret_key      => ENV['SECRET_KEY']
 	}
 
 	Stripe.api_key = Rails.configuration.stripe[:secret_key]
