b252da1a75122ed4f7a0bb4ab9582bbbf7820376
TomGroombridge <groombridgetom@gmail.com>
2015-02-23 12:56:21 +0000

    added different environments for the secret keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,10 @@
-Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
-}
+if Rails.env.development?
+	Stripe.api_key = Rails.application.secrets.secret_key
+else
+	Rails.configuration.stripe = {
+	  :publishable_key => ENV['PUBLISHABLE_KEY'],
+	  :secret_key      => ENV['SECRET_KEY']
+	}
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+	Stripe.api_key = Rails.configuration.stripe[:secret_key]
+end
