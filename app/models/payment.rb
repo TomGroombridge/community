afd277317c167758d99573aa64477d0dd9ada24a
TomGroombridge <groombridgetom@gmail.com>
2015-02-23 15:22:40 +0000

    reset

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -26,16 +26,21 @@ class Payment < ActiveRecord::Base
 		@course_name = self.course_date.course.name
 		@course_date = self.course_date.start_date.strftime("%d/%m/%Y")		
 
-		
+		if course_date.course.free?
+			save! and return true
+		end
+
 	  if valid?
+	  	begin
 	  		customer = Stripe::Customer.create(card: stripe_card_token, email: @email, description: @name)	    
 		    self.stripe_customer_token = customer.id	    	    
 		    Stripe::Charge.create(amount: @amount.to_i, currency: "gbp", customer: customer.id, description: "this is a payment for the #{@course_name} course on the #{@course_date}" )
 		    save!
-		end	    	
   		rescue Stripe::InvalidRequestError => e
     		logger.error "Stripe error while creating customer: #{e.message}"
 			  errors.add :base, "There was a problem with your credit card."
+			end	    	
+	  end
 	end
 
 	def send_reminder				
