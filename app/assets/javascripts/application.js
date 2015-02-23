6fe878c21b3a1f1467fa75c6226c2a90f8f8bff8
TomGroombridge <groombridgetom@gmail.com>
2015-02-23 13:47:45 +0000

    moved payments query into application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,6 @@
 //= require ckeditor/init
 
 
-
 $(document).ready(function() {  
 
     $('#example2')
@@ -84,7 +83,50 @@ $(document).ready(function() {
 
 
     var button = document.getElementById('upload_button');
-    var input  = document.getElementById('course_image');               
+    var input  = document.getElementById('course_image');      
+
+
+    var payment;
+
+    jQuery(function() {
+      Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+      return payment.setupForm();
+    });
+
+    payment = {
+      setupForm: function() {
+        return $('#new_payment').submit(function() {
+          $('input[type=submit]').attr('disabled', true);
+          if ($('#card_number').length) {
+            payment.processCard();
+            return false;
+          } else {
+            return true;
+          }
+        });
+      },
+      processCard: function() {
+        var card;
+        card = {
+          number: $('#card_number').val(),
+          cvc: $('#card_code').val(),
+          expMonth: $('#card_month').val(),
+          expYear: $('#card_year').val()
+        };
+        return Stripe.createToken(card, payment.handleStripeResponse);
+      },
+      handleStripeResponse: function(status, response) {
+        if (status === 200) {
+          $('#payment_stripe_card_token').val(response.id);
+          return $('#new_payment')[0].submit();
+        } else {
+          $('#stripe_error').text(response.error.message);
+          return $('input[type=submit]').attr('disabled', false);
+        }
+      }
+    };
+
+         
 
 
 });
