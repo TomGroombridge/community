d267d29f1fb7086e15842f0870778a3687380b94
TomGroombridge <groombridgetom@gmail.com>
2015-02-26 00:29:57 +0000

    fixed issue with current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,23 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_devise_permitted_parameters, if: :devise_controller?  
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
   protected
 
   #allows us to add extra attributes to the sign up process such as name
 
   def configure_devise_permitted_parameters
-    
+
     registration_params = [:first_name, :last_name, :email, :password, :password_confirmation, :type, :company_name, :subscription, :avatar, :company_website, :facebook, :twitter, :instagram, :google, :pinterest, :admin]
 
     if params[:action] == 'update'
-      devise_parameter_sanitizer.for(:account_update) { 
+      devise_parameter_sanitizer.for(:account_update) {
         |u| u.permit(registration_params << :current_password)
       }
     elsif params[:action] == 'create'
-      devise_parameter_sanitizer.for(:sign_up) { 
-        |u| u.permit(registration_params) 
+      devise_parameter_sanitizer.for(:sign_up) {
+        |u| u.permit(registration_params)
       }
     end
   end
