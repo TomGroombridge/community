e5ecfae49c01938706045eb2746ac74d36371142
TomGroombridge <groombridgetom@gmail.com>
2015-02-27 13:52:03 +0000

    added outline to sold out banner

--- app/controllers/course_dates_controller.rb
+++ app/controllers/course_dates_controller.rb
@@ -2,13 +2,13 @@ class CourseDatesController < ApplicationController
 
 	before_action :fetch_and_authorize_course
 
-	def new	
+	def new
 		@course_date = @course.course_dates.build
 	end
 
-	def create 			
-		@course_date = @course.course_dates.create(params[:course_date].permit(:start_date, :start_time, :end_date, :end_time, :course_id, :quantity))	
-		if @course_date.save								
+	def create
+		@course_date = @course.course_dates.create(params[:course_date].permit(:start_date, :start_time, :end_date, :end_time, :course_id, :quantity))
+		if @course_date.save
 			redirect_to dashboard_path(@course_date.course.user.id)
 		else
 			raise "error"
@@ -16,20 +16,20 @@ class CourseDatesController < ApplicationController
 	end
 
 	def edit
-		@course_date = CourseDate.find(params[:id])    
+		@course_date = CourseDate.find(params[:id])
 	end
 
-	def course_details		
+	def course_details
 		@course_date = @course.course_dates.find(params[:id])
-		@payments = @course_date.payments				
+		@payments = @course_date.payments
 		@percentage = @payments.count.to_f / @course_date.quantity.to_f
 		gon.percentage = @percentage
 	end
 
-	def update 
-		@course_date = CourseDate.find(params[:id])		
-		if @course_date.update_attributes(params[:course_date].permit(:start_date, :start_time, :end_date, :end_time, :course_id, :quantity))							
-			redirect_to course_details_course_course_date_path(@course_date.course_id, @course_date.id)			
+	def update
+		@course_date = CourseDate.find(params[:id])
+		if @course_date.update_attributes(params[:course_date].permit(:start_date, :start_time, :end_date, :end_time, :course_id, :quantity))
+			redirect_to course_details_course_course_date_path(@course_date.course_id, @course_date.id)
 		else
 			render :edit
 		end
