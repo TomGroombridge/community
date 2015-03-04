684af16a7e909725c274cfc521ae15c97dcfef10
TomGroombridge <groombridgetom@gmail.com>
2015-03-04 14:05:58 +0000

    reduced delay on pop up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
         hoverable: true,
         position : 'bottom left',
         delay: {
-          show: 300,
+          show: 100,
           hide: 800
         }
       })
@@ -91,7 +91,7 @@ $(document).ready(function() {
         hoverable: true,
         position : 'bottom left',
         delay: {
-          show: 300,
+          show: 100,
           hide: 800
         }
       })
