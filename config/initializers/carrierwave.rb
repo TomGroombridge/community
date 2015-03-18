bc845d7d13873c7be165db892d2fe7ff498b68c3
TomGroombridge <groombridgetom@gmail.com>
2015-03-18 10:35:28 +0000

    changed assets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
 	  config.fog_credentials = {
     	:provider              => 'AWS',
 	    :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-	    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+	    :aws_secret_access_key => ENV['aws_secret_access_key'],
 	    :region                => "eu-west-1",
 	  }
 	  config.storage = :fog
