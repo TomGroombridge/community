b620ceb5a98e0051c7baca94511df87066c0f594
TomGroombridge <groombridgetom@gmail.com>
2015-03-19 12:27:25 +0000

    added previous dates table with info

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,21 +1,21 @@
 class Course < ActiveRecord::Base
 	belongs_to :user
 	has_many :course_dates
-	has_many :users		  
+	has_many :users
   has_many :course_addresses
-  accepts_nested_attributes_for :course_addresses  
+  accepts_nested_attributes_for :course_addresses
   accepts_nested_attributes_for :course_dates
   mount_uploader :avatar, AvatarUploader
-  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h 
+  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
   after_update :crop_avatar
 
   def crop_avatar
-    self.avatar.recreate_versions! if crop_x.present? 
+    self.avatar.recreate_versions! if crop_x.present?
   end
 
  	def check_last_bookings
  		tomorrow_date = DateTime.now + 1
- 		tomorrow = tomorrow_date.strftime("%m/%d/%Y") 		
+ 		tomorrow = tomorrow_date.strftime("%m/%d/%Y")
  	end
 
   def free?
@@ -31,23 +31,29 @@ class Course < ActiveRecord::Base
   end
 
   def upcoming_date
-    active_course_dates = self.course_dates.select { |num|  num.active?  } 
+    active_course_dates = self.course_dates.select { |num|  num.active?  }
     sort_dates = active_course_dates.sort_by! {|obj| obj.start_date_time}
     sort_dates.first
   end
 
   def upcoming_dates
-    active_course_dates = self.course_dates.select { |num|  num.active?  } 
-    sort_dates = active_course_dates.sort_by! {|obj| obj.start_date_time}    
+    active_course_dates = self.course_dates.select { |num|  num.active?  }
+    sort_dates = active_course_dates.sort_by! {|obj| obj.start_date_time}
   end
 
-  def address    
+  def previous_dates
+    unactive_course_dates = self.course_dates.select { |num|  num.active == false  }
+    sort_dates = unactive_course_dates.sort_by! {|obj| obj.start_date_time}
+    sort_dates.reverse
+  end
+
+  def address
     course = ""
     course_addresses.each do |address|
       course << address.address1 + ', ' + address.address2  + ', ' + address.postcode
-    end   
-    course 
+    end
+    course
   end
-  
+
 end
 
