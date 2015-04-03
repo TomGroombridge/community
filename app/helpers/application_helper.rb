49caf6eaa7f567a8a476325c3bfe9e3ab01c2414
TomGroombridge <groombridgetom@gmail.com>
2015-04-03 16:53:33 +0100

    changed name of helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
 
-  def link_to_addd_fields(name, form, association)
+  def link_to_add_tickets(name, form, association)
     new_object = form.object.send(association).klass.new
     id = new_object.object_id
     fields = form.fields_for(association, new_object, child_index: id) do |builder|
