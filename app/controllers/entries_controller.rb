6fc4999cf38d0b024575b07e6482400287621ccb
TomGroombridge <groombridgetom@gmail.com>
2015-04-07 00:17:46 +0100

    a slection can be created with the course dates id

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,13 +3,12 @@ class EntriesController < ApplicationController
 
 	def new
 		@entry = @ticket.entries.build
-		@entry.entry_selections.build
-		# @entry.entry_selections.build
+		3.times{@entry.entry_selections.build}
+		@course_datess = @ticket.course_date.course.course_dates
 	end
 
 	def create
 		@entry = @ticket.entries.create(params[:entry].permit(:ticket_id, entry_selections_attributes:[:course_date_id, :entry_id]))
-		# @entry_selections = @entry.entry_selections.create(params[:entry].permit(:course_date_id))
 		raise @entry.entry_selections.inspect
 	end
 
