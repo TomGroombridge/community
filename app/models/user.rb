c1ed9ead48298c30cea8568a53c2922e88ba7ade
TomGroombridge <groombridgetom@gmail.com>
2015-04-06 11:33:48 +0100

    added new booking summary table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,23 @@
 class User < ActiveRecord::Base
- 	has_many :payments  
+ 	has_many :payments
  	has_many :courses
  	has_one :subscription
- 	has_one :dashboard 	
+ 	has_one :dashboard
  	mount_uploader :avatar, AvatarUploader
-  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h 
+  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
   after_update :crop_avatar
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def crop_avatar
-    self.avatar.recreate_versions! if crop_x.present? 
+    self.avatar.recreate_versions! if crop_x.present?
   end
 
   def full_name
     self.first_name + " " + self.last_name
   end
-
-
 
 end
