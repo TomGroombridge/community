e27c3b810c6679f8ffe14046d87142cc8a4373c6
TomGroombridge <groombridgetom@gmail.com>
2015-04-10 16:41:14 +0100

    testing sending reminder through booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,13 @@ class Booking < ActiveRecord::Base
 	belongs_to :payment
 	has_many :booking_dates
 	accepts_nested_attributes_for :booking_dates
+	after_create :send_reminder
+
+	def send_reminder
+		@course_date = self.payment.ticket.course_date.start_date_time
+		PaymentMailer.reminder(self.payment).deliver!
+	end
+
+
 
 end
