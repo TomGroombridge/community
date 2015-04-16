70a429896580961dd2b8d688395fa5e84a114fa3
TomGroombridge <groombridgetom@gmail.com>
2015-04-16 11:25:43 +0100

    added mailer sending email to myself to request deposit of funds

--- app/mailers/dashboard_mailer.rb
+++ app/mailers/dashboard_mailer.rb
@@ -2,6 +2,7 @@ class DashboardMailer < ActionMailer::Base
 	default from: "hello@courseiam"
 
 	def withdraw_payments(user)
+		@user = user
 		email = user.email
 		mail to: "groombridgetom@gmail.com", subject: "Withdraw Payments"
 	end
