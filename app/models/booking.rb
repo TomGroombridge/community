440a311bd46b4ff144be0eff042e2a6971a626b7
TomGroombridge <groombridgetom@gmail.com>
2015-04-30 13:19:37 +0100

    added reminder create on booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,7 @@ class Booking < ActiveRecord::Base
 	belongs_to :payment
 	has_many :booking_dates
 	accepts_nested_attributes_for :booking_dates
-	# after_create :send_reminder
+	after_create :send_reminder
 
 	def send_reminder
 		@course_date = self.payment.ticket.course_date.start_date_time
