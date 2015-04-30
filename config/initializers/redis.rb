809218b3abb90f0c451a56fa206d500c4413286b
TomGroombridge <groombridgetom@gmail.com>
2015-04-30 18:48:55 +0100

    revert back

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 encoded_url = URI.encode('redis://redistogo:92af4ff10756396e62e4c3e20b1d1d85@jack.redistogo.com:11477/')
 uri = URI.parse(encoded_url)
-# REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
 
