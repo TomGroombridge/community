0217f53604e07ee24fe91d2daa311b067edb7c4d
TomGroombridge <groombridgetom@gmail.com>
2015-05-02 16:45:38 +0100

    added a new booking reminder email instance

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,26 +1,25 @@
 class BookingsController < ApplicationController
-	before_action :fetch_and_authorize_ticket, :except => [:index]
+	# before_action :fetch_and_authorize_ticket, :except => [:index]
 
 	def new
-		@order = Order.create(params[:order])
-		@order.ticket_id = @ticket.id
-		@bookings = @order.bookings.build
-		# raise @bookings.inspect
-		# @order.bookings.build #add n.times{} depedning on the number of bookings being made
-		@order.bookings.each {|booking| 3.times{booking.booking_dates.build}}
-		# raise @order.bookings.last.booking_dates.inspect
-		# raise @order.bookings.inspect
-		@course_dates = @ticket.course_date.course.course_dates
-		@course_dates.map do |cd|
-			cd.start_time ==  cd.start_date_time.strftime("%A, %d %b %Y %l:%M %p")
-		end
-		@course_date = @ticket.course_date
+		@booking = Booking.new
+		@booking_dates = @booking.booking_dates.build
+	end
+
+	def create
+		@booking = Booking.new(booking_params)
+		@booking.save
+		raise @booking.inspect
 	end
 
 	private
 
-	def fetch_and_authorize_ticket
-		@ticket = Ticket.find(params[:ticket_id])
+	# def fetch_and_authorize_ticket
+	# 	@ticket = Ticket.find(params[:ticket_id])
+	# end
+
+	def booking_params
+    params.require(:booking).permit(:order_id, :name, :email, :contact_number, :special_request, booking_dates_attributes:[:booking_id, :course_date_id])
 	end
 
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,9 +7,9 @@ class Booking < ActiveRecord::Base
 
 	def send_reminder
 		@booking = self
-		@course_date = self.payment.ticket.course_date.start_date_time
-		PaymentMailer.delay_until(@course_date - 1.days).reminder(@booking.id)
-		# PaymentMailer.delay_for(10.seconds).reminder(self)#
+		self.booking_dates.each do |date|
+			PaymentMailer.delay_until(date.course_date.start_date_time - 1.days).reminder(@booking.id)
+		end
 	end
 
 	def self.search(search)
