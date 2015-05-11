d3591de8cc60fc36a5a2b15e8ce7ff39f046b031
TomGroombridge <groombridgetom@gmail.com>
2015-05-11 17:44:37 +0100

    testing send notification now

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -6,7 +6,7 @@ class Payment < ActiveRecord::Base
 	belongs_to :user
 	has_many :bookings
 	after_create :send_new_payment_email
-	after_create :send_notification
+	# after_create :send_notification
 	accepts_nested_attributes_for :bookings
 
 	attr_accessor :stripe_card_token
