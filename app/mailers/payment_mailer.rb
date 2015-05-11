c05ba38159fa407ed411a4b94d290c7edfff5b6a
TomGroombridge <groombridgetom@gmail.com>
2015-05-11 17:47:26 +0100

    changed course variable

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -8,8 +8,8 @@ class PaymentMailer < ActionMailer::Base
   end
 
   def notification(payment)
-  	@course = payment.ticket.course_date.course
     @booking_dates = payment.order.bookings.last.booking_dates
+  	@course = @booking_dates.last.course_date.course
     @payment = payment
   	mail to: @course.user.email, subject: "Someone has joined a class"
   end
