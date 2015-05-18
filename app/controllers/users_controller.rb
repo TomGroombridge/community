11c6512e32d39e1c3198a951118aab7763c24cff
TomGroombridge <groombridgetom@gmail.com>
2015-05-18 14:36:16 +0100

    got rid of active course dates in the calendar and just have course dates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,8 +52,7 @@ class UsersController < ApplicationController
 	  @courses = @user.courses
 	  @course_dates = []
 	  @courses.each {|c| c.course_dates.each {|cd| @course_dates << cd}}
-	  @active_course_dates = @course_dates.select {|c| c.valid_dates(c)}
-	  @course_dates = @active_course_dates.sort_by do |course_date|
+	  @course_dates = @courses.sort_by do |course_date|
 			course_date.start_date_time
 		end
 		@calender_dates = @course_dates.group_by(&:start_date)
