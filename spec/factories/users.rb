b5e73b961919abaedbf6efebc924d7c10017ae37
TomGroombridge <groombridgetom@gmail.com>
2015-08-28 10:01:11 +0100

    added all dmc solutions

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   factory :user do
   	first_name 'tom'
     last_name 'groombridge'
-    email 'tom@t.com'
+    email 'groombridge@gmail.com'
     contact_number "01285 6452968"
     company_name "Courseiam"
     password '123456789'
