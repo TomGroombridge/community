61e5fc18a384c45f7e6dc4aac22a1e59f4c3477a
TomGroombridge <groombridgetom@gmail.com>
2015-08-29 12:08:45 +0100

    added database cleaner to the spec helper and got the first user sign up test passing

--- spec/features/user_sign_up_feature_spec.rb
+++ spec/features/user_sign_up_feature_spec.rb
@@ -1,19 +1,24 @@
 require 'spec_helper'
-	# require_relative '../test_helpers/create_case_helper_spec'
 
-	# RSpec.configure do |c|
-	#   c.include CreateCaseHelper
-	# end
+	describe "sign up" do
 
+		context "given a home page" do
 
-	 describe "sign up" do
-	 		context "given a home page" do
-	 			it "should be able to navigate to the sign up page and sign up " do
-	 				visit "/"
-	 				click_link "signUp"
-	 				save_and_open_page
-	 				fill_in "user_first_name", with: "Tom"
-	 				fill_in "user_last_name", with: "Groombridge"
-	 			end
-	 		end
-	 end
+			it "should be able to navigate to the sign up page and sign up with correct details" do
+				visit "/"
+				click_link "signUp"
+				fill_in "user_first_name", with: "Tom"
+				fill_in "user_last_name", with: "Groombridge"
+				fill_in "signUpEmail", with: "groombridegtom@gmail.com"
+				fill_in "user_contact_number", with: "07777 777777"
+				fill_in "signUpPassword", with: "123456789"
+				fill_in "signUpPasswordConfirmation", with: "123456789"
+				fill_in "user_company_name", with: "Courseiam"
+				click_button "Sign Up"
+				expect(current_path).to eq("/")
+				expect(page).to have_content("Add Course")
+			end
+
+		end
+
+	end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ ENV["RAILS_ENV"] ||= 'test'
   require 'capybara/rspec'
   require 'capybara/rails'
   require 'factory_girl_rails'
+  require 'database_cleaner'
 # Given that it is always loaded, you are encouraged to keep this file as
 # light-weight as possible. Requiring heavyweight dependencies from this file
 # will add to the boot time of your test suite on EVERY test run, even for an
@@ -25,7 +26,22 @@ ENV["RAILS_ENV"] ||= 'test'
 RSpec.configure do |config|
   config.include Capybara::DSL
   config.include FactoryGirl::Syntax::Methods
+  #   Shoulda::Matchers.configure do |config|
+  #   config.integrate do |with|
+  #     # Choose a test framework:
+      # with.test_framework :rspec
+  #     # with.test_framework :minitest
+  #     # with.test_framework :minitest_4
+  #     # with.test_framework :test_unit
 
+  #     # Choose one or more libraries:
+  #     # with.library :active_record
+  #     # with.library :active_model
+  #     # with.library :action_controller
+  #     # Or, choose the following (which implies all of the above):
+  #     with.library :rails
+  #   end
+  # end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -47,52 +63,61 @@ RSpec.configure do |config|
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
-
   end
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
-=begin
-  # These two settings work together to allow you to limit a spec run
-  # to individual examples or groups you care about by tagging them with
-  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
-  # get run.
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
+  config.use_transactional_fixtures = true
 
-  # Limits the available syntax to the non-monkey patched syntax that is recommended.
-  # For more details, see:
-  #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
-  #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
-  #   - http://myronmars.to/n/dev-blog/2014/05/notable-changes-in-rspec-3#new__config_option_to_disable_rspeccore_monkey_patching
-  config.disable_monkey_patching!
-
-  # Many RSpec users commonly either run the entire suite or an individual
-  # file, and it's useful to allow more verbose output when running an
-  # individual spec file.
-  if config.files_to_run.one?
-    # Use the documentation formatter for detailed output,
-    # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
-    config.default_formatter = 'doc'
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
   end
 
-  # Print the 10 slowest examples and example groups at the
-  # end of the spec run, to help surface which specs are running
-  # particularly slow.
-  config.profile_examples = 10
+  config.before(:each) do
+    DatabaseCleaner.start
+    Capybara.current_driver = :rack_test
+    #MemFs.activate!
+  end
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = :random
+  config.after(:each) do
+    DatabaseCleaner.clean
+    #MemFs.deactivate!
+  end
 
-  # Seed global randomization in this process using the `--seed` CLI option.
-  # Setting this allows you to use `--seed` to deterministically reproduce
-  # test failures related to randomization by passing the same `--seed` value
-  # as the one that triggered the failure.
-  Kernel.srand config.seed
-=end
-
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
+  #   # These two settings work together to allow you to limit a spec run
+  #   # to individual examples or groups you care about by tagging them with
+  #   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
+  #   # get run.
+  #   config.filter_run :focus
+  #   config.run_all_when_everything_filtered = true
+  #   # Limits the available syntax to the non-monkey patched syntax that is recommended.
+  #   # For more details, see:
+  #   #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
+  #   #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+  #   #   - http://myronmars.to/n/dev-blog/2014/05/notable-changes-in-rspec-3#new__config_option_to_disable_rspeccore_monkey_patching
+  #   config.disable_monkey_patching!
+  #   # Many RSpec users commonly either run the entire suite or an individual
+  #   # file, and it's useful to allow more verbose output when running an
+  #   # individual spec file.
+  #   if config.files_to_run.one?
+  #     # Use the documentation formatter for detailed output,
+  #     # unless a formatter has already been configured
+  #     # (e.g. via a command-line flag).
+  #     config.default_formatter = 'doc'
+  #   end
+  #   # Print the 10 slowest examples and example groups at the
+  #   # end of the spec run, to help surface which specs are running
+  #   # particularly slow.
+  #   config.profile_examples = 10
+  #   # Run specs in random order to surface order dependencies. If you find an
+  #   # order dependency and want to debug it, you can fix the order by providing
+  #   # the seed, which is printed after each run.
+  #   #     --seed 1234
+  #   config.order = :random
+  #   # Seed global randomization in this process using the `--seed` CLI option.
+  #   # Setting this allows you to use `--seed` to deterministically reproduce
+  #   # test failures related to randomization by passing the same `--seed` value
+  #   # as the one that triggered the failure.
+  #   Kernel.srand config.seed
 end
