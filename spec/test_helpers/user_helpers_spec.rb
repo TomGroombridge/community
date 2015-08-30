0c6a81d76b975dfdb3631e84e1816f0687b760e5
TomGroombridge <groombridgetom@gmail.com>
2015-08-30 12:09:38 +0100

    added test for creating a course when you are sign in as a admin user

--- spec/test_helpers/user_helpers_spec.rb
+++ spec/test_helpers/user_helpers_spec.rb
@@ -2,6 +2,10 @@ module CreateUserHelper
 
 	def user_sign_in
 		visit "/"
+		click_link "signIn"
+		fill_in "user_email", with: "#{@user.email}"
+		fill_in "user_password", with: "#{@user.password}"
+    click_button('signIn')
 	end
 
 end
