132743e19849a5d9ed88db9c4815f4ec6a73f053
TomGroombridge <groombridgetom@gmail.com>
2015-08-30 11:08:22 +0100

    added some rough js to make sure that you can submit the sign up form using the enter key

--- spec/features/user_sign_up_feature_spec.rb
+++ spec/features/user_sign_up_feature_spec.rb
@@ -19,6 +19,19 @@ require 'spec_helper'
 				expect(page).to have_content("Add Course")
 			end
 
+			it "should not be able to sign in if all the fields aren't filled out", :js => true do
+				visit "/"
+				click_link "signUp"
+				fill_in "user_first_name", with: "Tom"
+				fill_in "user_last_name", with: "Groombridge"
+				fill_in "signUpEmail", with: "groombridegtom@gmail.com"
+				fill_in "signUpPassword", with: "123456789"
+				fill_in "signUpPasswordConfirmation", with: "123456789"
+				fill_in "user_company_name", with: "Courseiam"
+				save_and_open_page
+				click_button "Sign Up"
+			end
+
 		end
 
 	end
---
+++ spec/test_helpers/user_helpers_spec.rb
@@ -0,0 +1,7 @@
+module CreateUserHelper
+
+	def user_sign_in
+		visit "/"
+	end
+
+end
